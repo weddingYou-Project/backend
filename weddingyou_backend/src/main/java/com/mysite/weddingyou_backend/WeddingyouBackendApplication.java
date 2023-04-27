fe1756f250288b04cfcd29ac69c2919c62c50403
diana815 <diana815@naver.com>
2023-04-27 10:18:42 +0900

    feat : 기본 설정 파일 설정

---
+++ weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/WeddingyouBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.weddingyou_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingyouBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingyouBackendApplication.class, args);
+	}
+
+}
---
+++ weddingyou_backend/src/test/java/com/mysite/weddingyou_backend/WeddingyouBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.weddingyou_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingyouBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
