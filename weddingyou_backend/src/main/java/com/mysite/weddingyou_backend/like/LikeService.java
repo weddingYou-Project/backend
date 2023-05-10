73da28fc03134ed0838f933ebcd23cc065d33d66
diana815 <diana815@naver.com>
2023-05-10 22:17:58 +0900

    fix : likeService update

--- weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/like/LikeService.java
+++ weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/like/LikeService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.mysite.weddingyou_backend.item.Item;
-import com.mysite.weddingyou_backend.userLogin.UserLogin;
 
 @Service
 @Transactional
@@ -16,10 +14,10 @@ public class LikeService {
 	@Autowired
 	LikeRepository likeRepository;
 	
-//	//찜목록 조회
-//	public List<LikeEntity> getLikeList(Long userId) {
-//        return likeRepository.findByUserId(userId);
-//    }
+	//찜목록 조회
+	public List<LikeEntity> getLikeList(String email) {
+        return likeRepository.findByEmail(email);
+    }
 
 	//좋아요 추가
     public void addLike(LikeEntity likeEntity) {
@@ -30,16 +28,11 @@ public class LikeService {
     public void deleteLike(Long likeId) {
         likeRepository.deleteById(likeId);
     }
-
+    
     //필터링
-//    public List<LikeEntity> getLikeListByCategory(Long userId, String category1, String category2) {
-//    	LikeEntity likeentity = new LikeEntity();
-//    	UserLogin user = new UserLogin();
-//    	user.setUserId(userId);
-//    	likeentity.setUserId(user);
-//    	Item item = likeentity.getItem();
-//        return likeRepository.findByUserIdAndItemId_Category();
-//    }
+    public List<LikeEntity> getLikeListByCategory(String email, String category) {
+        return likeRepository.findByEmailAndItemId_Category(email, category);
+    }
     
     //정렬(가나다순, 인기순, 지역순)
 
