0483e9f42e918cedba0da50102351ff8316d12fa
diana815 <diana815@naver.com>
2023-05-10 22:19:07 +0900

    fix : likeController update

--- weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/like/LikeController.java
+++ weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/like/LikeController.java
@@ -1,7 +1,7 @@
 package com.mysite.weddingyou_backend.like;
 
 import java.util.Collections;
-import java.util.Comparator;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mysite.weddingyou_backend.item.ItemService;
 import com.mysite.weddingyou_backend.userLogin.UserLogin;
 import com.mysite.weddingyou_backend.userLogin.UserLoginRepository;
-import com.mysite.weddingyou_backend.userLogin.UserLoginService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -35,29 +34,29 @@ public class LikeController {
 	@Autowired
 	private ItemService itemService;
 	
-//	//찜목록 조회
-//	@GetMapping("/list")
-//    public List<LikeEntity> getLikeList(HttpServletRequest request) {
-//        HttpSession session = request.getSession();
-//        UserLogin loggedInUser = (UserLogin) session.getAttribute("loggedInUser");
-//        List<LikeEntity> likeList = likeService.getLikeList(Long.valueOf(loggedInUser.getUserId()));
-//        return likeList;
-//    }
-//	
-//	//좋아요 생성
-//	@PostMapping("/create")
-//	public ResponseEntity<Void> createLike(@RequestParam Long itemId, HttpServletRequest request) {
-//	    HttpSession session = request.getSession();
-//	    UserLogin loggedInUser = (UserLogin) session.getAttribute("loggedInUser");
-//
-//	    LikeEntity likeEntity = new LikeEntity();
-//	    likeEntity.setEmail(userRepository.findByEmail(loggedInUser.getEmail()));
-//	    likeEntity.setItemId(itemService.getItemById(itemId));
-//
-//	    likeService.addLike(likeEntity);
-//	    return ResponseEntity.ok().build();
-//	}
-//	
+	//찜목록 조회
+	@GetMapping("/list")
+    public List<LikeEntity> getLikeList(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        UserLogin loggedInUser = (UserLogin) session.getAttribute("loggedInUser");
+        List<LikeEntity> likeList = likeService.getLikeList(loggedInUser.getEmail());
+        return likeList;
+    }
+	
+	//좋아요 생성
+	@PostMapping("/create")
+	public ResponseEntity<Void> createLike(@RequestParam Long itemId, HttpServletRequest request) {
+	    HttpSession session = request.getSession();
+	    UserLogin loggedInUser = (UserLogin) session.getAttribute("loggedInUser");
+
+	    LikeEntity likeEntity = new LikeEntity();
+	    likeEntity.setEmail(userRepository.findByEmail(loggedInUser.getEmail()));
+	    likeEntity.setItemId(itemService.getItemById(itemId));
+
+	    likeService.addLike(likeEntity);
+	    return ResponseEntity.ok().build();
+	}
+	
 	//좋아요 삭제
 	@DeleteMapping("/delete/{likeId}")
 	public ResponseEntity<Void> deleteLike(@PathVariable Long likeId) {
@@ -65,40 +64,39 @@ public class LikeController {
 		return ResponseEntity.ok().build();
 	}
 	
-//	//필터링
-//	@GetMapping("/list/{category}")
-//	public List<LikeEntity> getLikeListByCategory(HttpServletRequest request, @RequestParam("category1") String category1, @RequestParam("category2") String category2 ) {
-//	    HttpSession session = request.getSession();
-//	    UserLogin loggedInUser = (UserLogin) session.getAttribute("loggedInUser");
-//	    List<LikeEntity> likeList = likeService.getLikeListByCategory(Long.valueOf(loggedInUser.getUserId()), category1, category2);
-//	    return likeList;
-//	}
 	
-//	//정렬(가나다순, 인기순, 지역순)
-//	@GetMapping("/list/sort")
-//	public List<LikeEntity> getLikeList(HttpServletRequest request, @RequestParam(required = false) String sortBy) {
-//	    HttpSession session = request.getSession();
-//	    UserLogin loggedInUser = (UserLogin) session.getAttribute("loggedInUser");
-//	    List<LikeEntity> likeList = likeService.getLikeList(Long.valueOf(loggedInUser.getUserId()));
-//
-//	    if (sortBy != null) {
-//	        switch (sortBy) {
-//	            case "name": //오름차순
-//	            	Collections.sort(likeList, (a, b) -> a.getItemId().getItemName().compareTo(b.getItemId().getItemName()));
-//	                break;
-//	            case "popularity": //내림차순
-//	            	Collections.sort(likeList, (a, b) -> b.getLikeCount().compareTo(a.getLikeCount()));
-//	                break;
-//	            case "location": //오름차순
-//	                Collections.sort(likeList, (a, b) -> a.getLocation().compareTo(b.getLocation()));
-//	                break;
-//	            default:
-//	                // 예외 처리
-//	                throw new IllegalArgumentException("Invalid sort option: " + sortBy);
-//	        }
-//	    }
-//	    return likeList;
-//	}
+	//필터링
+	@GetMapping("/list/{category}")
+	public List<LikeEntity> getLikeListByCategory(HttpServletRequest request, @RequestParam("category") String category) {
+	    HttpSession session = request.getSession();
+	    UserLogin loggedInUser = (UserLogin) session.getAttribute("loggedInUser");
+	    List<LikeEntity> likeList = likeService.getLikeListByCategory(loggedInUser.getEmail(), category);
+	    return likeList;
+	}
+	
+	//정렬(가나다순, 인기순, 지역순)
+	@GetMapping("/list/sort")
+	public List<LikeEntity> getLikeList(HttpServletRequest request, @RequestParam(required = false) String sortBy) {
+	    HttpSession session = request.getSession();
+	    UserLogin loggedInUser = (UserLogin) session.getAttribute("loggedInUser");
+	    List<LikeEntity> likeList = likeService.getLikeList(loggedInUser.getEmail());
 
-
+	    if (sortBy != null) {
+	        switch (sortBy) {
+	            case "name": //오름차순
+	            	Collections.sort(likeList, (a, b) -> a.getItemId().getItemName().compareTo(b.getItemId().getItemName()));
+	                break;
+	            case "popularity": //내림차순
+	            	Collections.sort(likeList, (a, b) -> b.getLikeCount().compareTo(a.getLikeCount()));
+	                break;
+	            case "location": //오름차순
+	                Collections.sort(likeList, (a, b) -> a.getLocation().compareTo(b.getLocation()));
+	                break;
+	            default:
+	                // 예외 처리
+	                throw new IllegalArgumentException("Invalid sort option: " + sortBy);
+	        }
+	    }
+	    return likeList;
+	}
 }
