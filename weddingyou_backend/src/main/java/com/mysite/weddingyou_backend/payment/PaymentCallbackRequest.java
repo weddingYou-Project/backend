833bb65d1995d197d3d37969b6be70631776ede3
yeonsomin <sorina815@naver.com>
2023-05-18 11:10:06 +0900

    fix : 계약금 결제와 결제를 구분하도록 수정

--- weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/payment/PaymentCallbackRequest.java
+++ weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/payment/PaymentCallbackRequest.java
@@ -3,7 +3,8 @@ package com.mysite.weddingyou_backend.payment;
 public class PaymentCallbackRequest {
 	
     private Long paymentId;
-    private String status;
+    private PaymentStatus status;
+    private String paymentType;
 
     public Long getPaymentId() {
         return paymentId;
@@ -12,13 +13,40 @@ public class PaymentCallbackRequest {
     public void setPaymentId(Long paymentId) {
         this.paymentId = paymentId;
     }
-
-    public String getStatus() {
-        return status;
+    
+    public String getPaymentType() {
+        return paymentType;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
     }
 
+//    public String getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(String status) {
+//        this.status = status;
+//    }
+    
+    public enum PaymentStatus {
+        PAID("paid"),
+        CANCELLED("cancelled"),
+        OTHER("other");
+
+        private final String value;
+
+        PaymentStatus(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+	public PaymentStatus getStatus() {
+		return status;
+	}
 }
--- weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/payment/PaymentController.java
+++ weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/payment/PaymentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mysite.weddingyou_backend.payment.PaymentCallbackRequest.PaymentStatus;
 import com.siot.IamportRestClient.IamportClient;
 
 
@@ -27,42 +28,48 @@ public class PaymentController {
     public void handlePaymentCallback(@RequestBody PaymentCallbackRequest callbackRequest) {
         // 콜백 이벤트 처리 로직
     	Long paymentId = callbackRequest.getPaymentId();
-        String status = callbackRequest.getStatus();
+        PaymentStatus status = callbackRequest.getStatus();
+        String paymentType = callbackRequest.getPaymentType(); // 결제 종류 추가
         
-        //결제 상태에 따른 처리
-        if (status.equals("paid")) {
-            // 결제 완료 처리
-            processPaymentComplete(paymentId);
-        } else if (status.equals("cancelled")) {
-            // 결제 취소 처리
-            processPaymentCancel(paymentId);
+        
+        if (paymentType.equals("deposit")) {
+            // 계약금 결제 처리
+            processDepositPayment(paymentId, status.getValue());
         } else {
-            // 기타 상태 처리
-            processPaymentOther(paymentId);
+            // 일반 결제 처리
+        	processPayment(paymentId, status.getValue());
         }
     }
-
-    private void processPaymentComplete(Long paymentId) {
-        // 결제 완료 처리 로직
+    
+    private void processDepositPayment(Long paymentId, String status) {
+        // 계약금 결제 처리 로직
         Payment payment = paymentService.getPaymentById(paymentId);
-        payment.setPaymentStatus("completed");
-        payment.setPaymentDate(LocalDateTime.now());
+        payment.setDepositStatus(status);
+        payment.setDepositDate(LocalDateTime.now());
         paymentService.savePayment(payment);
     }
 
-    private void processPaymentCancel(Long paymentId) {
-        // 결제 취소 처리 로직
-    	Payment payment = paymentService.getPaymentById(paymentId);
-        payment.setPaymentStatus("cancelled");
-        payment.setPaymentDate(LocalDateTime.now());
-        paymentService.savePayment(payment);
-    }
-
-    private void processPaymentOther(Long paymentId) {
-        // 기타 상태 처리 로직
+    private void processPayment(Long paymentId, String status) {
+        // 결제 처리 로직
         Payment payment = paymentService.getPaymentById(paymentId);
-        payment.setPaymentStatus("other");
+        payment.setPaymentStatus(status);
         payment.setPaymentDate(LocalDateTime.now());
         paymentService.savePayment(payment);
     }
+
+//    private void processPaymentCancel(Long paymentId) {
+//        // 결제 취소 처리 로직
+//    	Payment payment = paymentService.getPaymentById(paymentId);
+//        payment.setPaymentStatus("cancelled");
+//        payment.setPaymentDate(LocalDateTime.now());
+//        paymentService.savePayment(payment);
+//    }
+//
+//    private void processPaymentOther(Long paymentId) {
+//        // 기타 상태 처리 로직
+//        Payment payment = paymentService.getPaymentById(paymentId);
+//        payment.setPaymentStatus("other");
+//        payment.setPaymentDate(LocalDateTime.now());
+//        paymentService.savePayment(payment);
+//    }
 }
