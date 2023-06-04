72d60d26d891073ea187b6a1515db4a979819f43
diana815 <diana815@naver.com>
2023-06-04 20:04:44 +0900

    merge : 성준님이 한 부분 comment, notice, qna 통합

---
+++ weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/comment/CommentDTO.java
@@ -0,0 +1,23 @@
+package com.mysite.weddingyou_backend.comment;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentDTO {
+    private Long commentId;
+    
+	private String commentWriter;
+
+    private String commentContent;
+
+    public static CommentDTO fromEntity(Comment comment) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setCommentId(comment.getCommentId());
+        commentDTO.setCommentWriter(comment.getCommentWriter());
+        commentDTO.setCommentContent(comment.getCommentContent());
+        return commentDTO;
+    }
+
+}
---
+++ weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/qna/Qna.java
@@ -0,0 +1,47 @@
+package com.mysite.weddingyou_backend.qna;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysite.weddingyou_backend.comment.Comment;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Qna")
+public class Qna {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "qna_id")
+    private long qnaId;
+
+    @Column(name = "qna_writer")
+    private String qnaWriter;
+
+    @Column(name = "qna_title")
+    private String qnaTitle;
+
+    @Column(name = "qna_content")
+    private String qnaContent;
+    
+	@Column(name = "qna_attachedfile")
+	private String qnaImg;
+
+    @Column(name = "qna_write_date")
+    private LocalDateTime qnaWriteDate;
+
+    @OneToMany(mappedBy = "qna", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+}
