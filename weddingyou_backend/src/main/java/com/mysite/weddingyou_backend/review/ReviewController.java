4d96d9e5d0d9c1f2fa2903873014227d2f5faa79
diana815 <diana815@naver.com>
2023-06-04 23:32:22 +0900

    fix : review 수정시 제목 수정 가능하도록 fix

--- weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/review/ReviewController.java
+++ weddingyou_backend/src/main/java/com/mysite/weddingyou_backend/review/ReviewController.java
@@ -154,4 +154,70 @@ public class ReviewController {
 		}
 		return targetReview;
     }
+	
+	@PostMapping(value = "/updatedreviews")
+	public int updateReview(@RequestParam("reviewText") String reviewText,
+			@RequestParam("reviewTitle") String reviewTitle,
+	        @RequestParam("reviewStars") Integer reviewStars,
+	        @RequestParam(value = "reviewImg", required = false) MultipartFile[] reviewImg,
+	        @RequestParam("userEmail") String userEmail,
+	        @RequestParam("plannerEmail") String plannerEmail,
+	        @RequestParam("estimateId") Long estimateId) throws IOException {
+		
+		int res = 0;
+	    
+	    // 파일 저장
+		List<String> list = new ArrayList<>();
+	 	if (!(reviewImg == null)) {
+	 	for (MultipartFile file : reviewImg) {
+	 		if (!file.isEmpty()) {
+	 			File storedFilename = new File(UUID.randomUUID().toString() + "_" + file.getOriginalFilename());
+	 			list.add("\"" + storedFilename.toString() + "\"");
+                file.transferTo(storedFilename); //업로드
+	 		}
+	 	}
+	 	}
+	 	
+	 	Review review = new Review();
+	 	if(reviewText.equals("undefined")) {
+	 		review.setReviewText("");
+	 	}else {
+	 		review.setReviewText(reviewText);
+	 	}
+	 	
+	 	review.setReviewStars(reviewStars);
+	 	review.setReviewImg(list.toString());
+	 	review.setUserEmail(userEmail);
+	 	review.setPlannerEmail(plannerEmail);
+	 	review.setReviewDate(LocalDateTime.now());
+	 	review.setReviewTitle(reviewTitle);
+	 	review.setEstimateId(estimateId);
+	 	PlannerUpdateDelete plannerData = plannerUpdateDeleteRepository.findByEmail(plannerEmail);
+	 	review.setReviewTitle(plannerData.getName()+"플래너 Review");
+	 	review.setReviewCounts(0);
+	 	System.out.println(review);
+	 	
+	 	if(reviewService.findEstimate(estimateId)!=null) {
+	 		Review targetReview = reviewService.findEstimate(estimateId);
+	 		targetReview.setPlannerEmail(plannerEmail);
+	 		targetReview.setReviewText(reviewText);
+	 		targetReview.setReviewStars(reviewStars);
+	 		targetReview.setReviewImg(list.toString());
+	 		targetReview.setUserEmail(userEmail);
+	 		targetReview.setReviewDate(LocalDateTime.now());
+	 		targetReview.setReviewTitle(reviewTitle);
+	 		reviewService.save(targetReview);
+	 		res =1;
+	 	}else {
+	 	// 리뷰 생성 및 데이터베이스 저장
+		    reviewService.save(review);
+		    res=1;
+	 	}
+	 	
+	 	
+	    
+	    
+	    return res;
+	    
+	}
 }
